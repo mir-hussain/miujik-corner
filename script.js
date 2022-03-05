3f3edd379a7eb4392c18327d58248e2e1ed62e78
mir-hussain <mirhussainmurtaza@gmail.com>
2022-03-05 19:13:02 +0600

    album added

--- script.js
+++ script.js
@@ -37,8 +37,39 @@ const showArtists = (data) => {
   </div>
   <button class="album-button">
     <i class="fa-solid fa-compact-disc"></i>
-    <p class="button-title">Albums</p>
+    <p onclick="fetchAlbums('${
+      artist?.idArtist
+    }')" class="button-title">Albums</p>
   </button>`;
     artistContainer.appendChild(div);
+  });
+};
+
+const fetchAlbums = (id) => {
+  const url = `https://theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showAlbum(data));
+};
+
+const showAlbum = ({ album }) => {
+  const albumContainer = elementById("albums");
+  album.forEach((item) => {
+    console.log(item);
+    const div = document.createElement("div");
+    div.classList.add("album");
+    div.innerHTML = `
+        <div class="album-image-container">
+          <img
+            src="${item.strAlbumThumb}"
+            alt=""
+          />
+        </div>
+        <div class="album-name">
+          <h3>${item.strAlbum}</h3>
+        </div>
+      `;
+
+    albumContainer.appendChild(div);
   });
 };
